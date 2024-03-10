7e5bd0608e2e41113256e7812907f7aa13db8ffa
asiffaizan <asiffaizan0077@gmail.com>
2024-03-10 11:26:22 +0600

    you-demy

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Nav/Nav.css'
+
+export default function Nav() {
+  const anchorStyle = 'px-7 cursor-pointer font-bold hover:text-[#C039F5] active:text-red-200 duration-300'
+  return (
+    <nav className='flex flex-row justify-between p-5 items-center border border-b-2'>
+        <a href="/">
+          <img src='/you-demy-logo.png' alt='YouDemy Logo' className='w-28' />
+        </a>
+        <div className='flex flex-row justify-between items-center'>
+            <a href='/' className={`active ${anchorStyle}`} checked>All Courses</a>
+            <a href='/' className={anchorStyle}>My Courses</a>
+            <a href='/' className={anchorStyle}>Overview</a>
+        </div>
+    </nav>
+  )
+}
